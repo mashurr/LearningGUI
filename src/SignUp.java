91aa78543c9d0efb95d2396ee46ed30348dfd7c0
mashur <mashur@local.com>
2021-04-28 19:32:44 +0530

    added boilerplate

---
+++ src/SignUp.java
@@ -0,0 +1,12 @@
+/**
+ * LearningGUI -- Homework 1 -- SignUp
+ * <p>
+ * Brief description of the program
+ * <p>
+ * a list of your sources of help (if any)
+ *
+ * @author Ayush Anand, lab sec OL1
+ * @version April 28, 2021
+ */
+public class SignUp {
+}
